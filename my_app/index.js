8afaf4a57416aa89ae7d907e9ccaff46094a49a0
eunho <jeh4916@gmail.com>
2023-09-16 13:27:14 +0900

    2-4. 서버 유지하기 - Nodemon

--- my_app/index.js
+++ my_app/index.js
@@ -8,14 +8,6 @@ const app = express();
 // middleware
 // main page GET
 app.get("/", (req, res) => {
-  // res.send("Main page GET request");
-  // res.send("<h1>Hello Main page</h1");
-  // res.send(`
-  // <h1>H1</h1>
-  // <h2>H2</h2>
-  // <h3>H3</h3>
-  // <h4>H4</h4>
-  // `);
   res.sendFile(__dirname + "/public/main.html");
 });
 
