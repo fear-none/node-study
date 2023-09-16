659ec308be3ae2840f69e2880b2d0ea66288c9dc
eunho <jeh4916@gmail.com>
2023-09-16 13:33:17 +0900

    3-2. 화면 구성하기 - Template Engine - 확장

--- my_app/index.js
+++ my_app/index.js
@@ -1,14 +1,28 @@
 import express from "express";
 import path from "path";
+import nunjucks from "nunjucks";
 
 const __dirname = path.resolve();
 
 const app = express();
 
+// view engine set
+app.set("view engine", "html"); // main.html -> main(.html)
+
+// nunjucks
+nunjucks.configure("views", {
+  watch: true, // html 파일이 수정될 경우, 다시 반영 후 렌더링
+  express: app,
+});
+
 // middleware
 // main page GET
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/write", (req, res) => {
+  res.render("write.html");
 });
 
 app.listen(3000, () => {
