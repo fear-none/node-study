b2130bc3be79fbb0e6f74890c06d3a45954d55a3
eunho <jeh4916@gmail.com>
2023-09-16 13:22:43 +0900

    2-2. 서버에서 파일 전송하기

--- my_app/index.js
+++ my_app/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
 // middleware
 // main page GET
 app.get("/", (req, res) => {
-  res.send("Main page GET request");
+  // res.send("Main page GET request");
+  // res.send("<h1>Hello Main page</h1");
+  // res.send(`
+  // <h1>H1</h1>
+  // <h2>H2</h2>
+  // <h3>H3</h3>
+  // <h4>H4</h4>
+  // `);
+  res.sendFile(__dirname + "/public/main.html");
 });
 
 app.listen(3000, () => {
